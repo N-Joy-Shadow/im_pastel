d4e818def1443dd4c097af99ff04f84b24c7e15e
Ich-mag-dich <san02@kakao.com>
2023-08-21 10:47:55 +0900

    버튼이이상해요

--- src/components/button/ButtonBase.tsx
+++ src/components/button/ButtonBase.tsx
@@ -1,25 +1,31 @@
 import React from "react"
 
 interface IButtonBaseProps {
-    href: string
-    name : string
-    primary_color : string
-    secondary_color : string
+  href: string
+  name: string
+  primary_color: string
+  secondary_color: string
 }
 
 export interface IButtonProps {
-    href : string;
-    primary_color : string
+  href: string;
+  primary_color: string
 }
 
-const ButtonBase = (props : IButtonBaseProps) => {
-  const buttonStyle = `w-[150px] my-2 h-[50px] px-[25px] py-[10px] text-center transition rounded-2xl border-[${props.primary_color}] text-[${props.secondary_color}] duration-300 border-2 
-xl:mx-2 xl:hover:border-white xl:hover:text-white xl:hover:cursor-pointer xl:text-[${props.primary_color}] xl:hover:bg-[${props.secondary_color}]
-`
+export interface IEtcButtonProps {
+  href: string;
+  name: string
+  primary_color: string
+  secondary_color: string
+}
+const ButtonBase = (props: IButtonBaseProps) => {
+  const buttonStyle = ` w-[150px] my-2 h-[50px] px-[25px] py-[10px] text-center transition rounded-2xl border-[${props.primary_color}] text-[${props.secondary_color}] duration-300 border-2 
+xl:mx-2 xl:hover:border-white xl:hover:text-white  xl:text-[${props.primary_color}] xl:hover:bg-[${props.secondary_color}] xl:hover:cursor-pointer `
   return (
     <a className={buttonStyle} href={props.href} target="blank">
-        {props.name}
+      {props.name}
     </a>
-)}
+  )
+}
 
 export default ButtonBase
